c9ad602ed9f8549d36734caeca6e4ba7326f1136
vladcherpak <cherpakvl@gmail.com>
2024-11-23 10:14:13 +0200

    burger-menu

---
+++ src/components/Layout/Header/Burgermenu/index.jsx
@@ -0,0 +1,31 @@
+"use client"
+import React, { useState }  from 'react'
+import Image from "next/image";
+import css from "./style.module.css"
+import  burger from "@/static/icons/Home.svg"
+const Burger = () => {
+    const [show, setShow] = useState(false);
+    const menuHandler = () => {
+        setShow(!show);
+        console.log(show);
+      };
+  return (
+    <>
+    <button onClick={() => menuHandler()}
+            className={css.menuBtn}>
+        <Image 
+            src={burger} width={24} height={24}/>
+    </button>
+        <ul className={show ? css.list + " " + css.active : css.list}>
+          <li className={css.item}> Home</li>
+          <li className={css.item}> Menu</li>
+          <li className={css.item}> About us</li>
+          <li className={css.item}> Order online</li>
+          <li className={css.item}> Reservation</li>
+          <li className={css.item}> Contact us</li>
+        </ul>
+    </>
+  )
+}
+
+export default Burger
--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import css from "./style.module.css"
 import Logo from './Logo'
+import cart from "@/static/icons/Cart.svg"
+import Image from "next/image";
+import Burger from './Burgermenu';
 const Header = () => {
   return (
-    <header className={css.container}>
+    <header className={css.header}>
     <div className={"container"}>
       <div className={css.wrapper}>
         <Logo/>
+        <div className={css.header__inner}>
+          <button>
+            <Image src={cart} width={24} height={24}/>
+          </button>
+          <Burger/>
+        </div>
       </div>
     </div>
     </header>
