98aeb6f877fbe11386a1e67d9420b82e9addf0b4
vladcherpak <cherpakvl@gmail.com>
2024-12-07 10:52:09 +0200

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Hero from "@/components/Layout/Header/Hero";
 import Welcome from "@/components/Layout/Header/Welcome";
+import Reserve from "@/components/Layout/Header/Reserve";
 
 export default function Home() {
   return (
     <>
     <Hero/>
     <Welcome/>
+    <Reserve/>
     </>
   );
 }
---
+++ src/components/Layout/Header/Reserve/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Image from "next/image";
+import css from "./style.module.css";
+import reserveImage from "@/static/reserveImage2.png";
+
+const Reserve = () => {
+  return (
+    <>
+      <section className={css.reserve}>
+        <div className={"container"}>
+          <div className={css.main}>
+            <div className={css.wrapper}>
+              <Image className={css.img} src={reserveImage} alt="Image" />
+            </div>
+            <div className={css.inner}>
+              <h2 className={css.title}>Let's reserve  <span className={css.span}> a table</span></h2>
+              <p className={css.text}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Facilisis ultricies at eleifend proin. Congue nibh nulla
+                malesuada ultricies nec quam{" "}
+              </p>
+              <button className={css.btn}>Reservation</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Reserve;
